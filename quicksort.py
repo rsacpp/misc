a95d32121ccbeda8b0b38977b5f4e05586ae0b8e
rsacpp <rsacpp@gmail.com>
2020-12-03 09:12:48 -0500

    fix style

--- quicksort.py
+++ quicksort.py
@@ -1,9 +1,11 @@
 import numpy as np
 
-def swap(arr, a , b):
+
+def swap(arr, a, b):
     if a == b:
         return
     arr[a], arr[b] = arr[b], arr[a]
+
 
 def quicksort(arr, left, right):
     if left >= right:
@@ -27,6 +29,7 @@ def quicksort(arr, left, right):
             quicksort(arr, left, rightpos)
             quicksort(arr, leftpos, right)
             break
+
 
 def quicksort2(arr, left, right):
     if left >= right:
@@ -53,14 +56,13 @@ def quicksort2(arr, left, right):
             quicksort2(arr, left, rightpos)
             quicksort2(arr, leftpos, right)
             break
-#arr = [1,2,3,4,5]
-#arr = [5,4,3,2,1]
+
+
 arr = np.random.rand(100)
-quicksort(arr,0, arr.size -1)
+quicksort(arr, 0, arr.size - 1)
 print(arr)
 
 arr2 = np.random.rand(100)
-#arr2 = range(6,1, -1)
 arr2 = list(arr2)
-quicksort2(arr2 ,0, len(arr2) -1)
+quicksort2(arr2, 0, len(arr2) - 1)
 print(arr2)
