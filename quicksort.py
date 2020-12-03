75880c430428ad9cb7e526ff5c6184667f3c818f
rsacpp <rsacpp@gmail.com>
2020-12-03 08:21:30 -0500

    version2

--- quicksort.py
+++ quicksort.py
@@ -27,10 +27,35 @@ def quicksort(arr, left, right):
             quicksort(arr, left, rightpos)
             quicksort(arr, leftpos, right)
             break
-            
+
+def quicksort2(arr, left, right):
+    if left >= right:
+        return
+    leftpos, rightpos = left, right-1
+    thevalue = arr.pop(right)
+    while leftpos < right:
+        while arr[leftpos] < thevalue:
+            leftpos += 1
+            if leftpos == right:
+                break
+        while arr[rightpos] > thevalue:
+            rightpos -= 1
+            if rightpos == left:
+                break
+        if leftpos < rightpos:
+            swap(arr, leftpos, rightpos)
+        else:
+            arr.insert(leftpos, thevalue)
+            quicksort2(arr, left, rightpos)
+            quicksort2(arr, leftpos, right)
+            break
 #arr = [1,2,3,4,5]
 #arr = [5,4,3,2,1]
-arr = np.random.rand(100000)
+arr = np.random.rand(100)
 quicksort(arr,0, arr.size -1)
-#swap(arr, 3, 4)
+print(arr)
+
+arr2 = np.random.rand(100)
+arr2 = list(arr2)
+quicksort2(arr2 ,0, len(arr2) -1)
 print(arr)
